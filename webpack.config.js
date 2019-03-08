05e7835897ff972a1b293f686d56e1790d560be5
Kevin Wang <wangkevin448@gmail.com>
2019-03-07 23:50:40 -0800

    Fix new courier prefix url

--- webpack.config.js
+++ webpack.config.js
@@ -116,14 +116,14 @@ const createConfig = (env, argv) => {
     config.plugins.push(
       new webpack.DefinePlugin({
         APIBASE_URL: JSON.stringify('http://localhost:8080/api'),
-        COURIERBASE_URL: JSON.stringify('http://localhost:8080/link'),
+        COURIERBASE_URL: JSON.stringify('http://localhost:8080/l'),
       }),
     );
   } else {
     config.plugins.push(
       new webpack.DefinePlugin({
         APIBASE_URL: JSON.stringify('/api'),
-        COURIERBASE_URL: JSON.stringify('/link'),
+        COURIERBASE_URL: JSON.stringify('/l'),
       }),
     );
     //config.plugins.push(new BundleAnalyzer({analyzerMode: 'static', openAnalyzer: true}));
